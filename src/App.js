8a032cb14408f4f59be4d15664804c218fc181d1
demitraps <109766095+demitraps@users.noreply.github.com>
2022-11-06 22:42:35 +0200

    Created footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
+import Signature from "./Signature";
 
 function App() {
   return (
     <div className="App">
       <div className="container-fluid">
         <Dictionary />
+        <Signature />
       </div>
     </div>
   );
---
+++ src/Signature.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Signature.css";
+
+export default function Signature() {
+  return (
+    <footer className="Signature">
+      <a
+        href="https://github.com/demitraps/dictionary-project"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open source code
+      </a>{" "}
+      by Demitra Ps
+    </footer>
+  );
+}
