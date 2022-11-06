bdba48b8a6e4d6432a74e7162df7ccbf4b5523c1
demitraps <109766095+demitraps@users.noreply.github.com>
2022-11-06 23:36:30 +0200

    Styled definitions and examples

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,41 +1,40 @@
 import React from "react";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
-    <div className="Meaning">
-      <div className="row mt-5">
-        <div className="col-2 border border-white border-opacity-25 pt-5 pb-5 pe-5 d-flex justify-content-end">
-          {props.meaning.partOfSpeech}
-        </div>
-        {props.meaning.definitions.map(function (definition, index) {
-          if (definition.example) {
-            return (
-              <div key={index} className="col-10">
-                <div className="border border-white border-opacity-25 pt-5 pb-5">
-                  <p>definition</p> <p>{definition.definition}</p>
-                </div>
-                <div className="border border-white border-opacity-25 pt-5 pb-5">
-                  <p>examples</p>
-                  <p>{definition.example}</p>
-                </div>
+    <div className="Meaning pt-5">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        if (definition.example) {
+          return (
+            <div key={index} className="row search-results">
+              <div className="col definition">
+                <p className="title">definition</p>
+                <p className="description">{definition.definition}</p>
               </div>
-            );
-          } else {
-            return (
-              <div key={index} className="col-10">
-                <div className="border border-white border-opacity-25 pt-5 pb-5">
-                  <p>definition</p> <p>{definition.definition}</p>
-                </div>
-                <div className="border border-white border-opacity-25 pt-5 pb-5">
-                  <p>examples</p>
-                  <p>No examples available</p>
-                </div>
+              <div className="col example">
+                <p className="title">examples</p>
+                <p className="text">"{definition.example}"</p>
               </div>
-            );
-          }
-        })}
-      </div>
+            </div>
+          );
+        } else {
+          return (
+            <div key={index} className="row search-results">
+              <div className="col definition">
+                <p className="title">definition</p>
+                <p className="description">{definition.definition}</p>
+              </div>
+              <div className="col example">
+                <p className="title">examples</p>
+                <p className="no-text">no examples available</p>
+              </div>
+            </div>
+          );
+        }
+      })}
     </div>
   );
 }
